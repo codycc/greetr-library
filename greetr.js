3207b41b08a05d7cffe6ffdeaa91f92faa0646a3
Cody Condon <codycondon50@gmail.com>
2016-03-28 11:28:51 -0400

    adding methods to prototype

--- greetr.js
+++ greetr.js
@@ -3,8 +3,66 @@
   var Greetr = function(firstName,lastName,language) {
     return new Greetr.init(firstName, lastName, language);
   }
+  var supportedLangs = ['en', 'es'];
 
-  Greetr.prototype = {};
+  var greetings = {
+    en: 'Hello',
+    es: 'Hola'
+  };
+
+  var formalGreetings = {
+    en: 'Greetings',
+    es: 'Saludos'
+  };
+
+  var logMessages = {
+    en: 'Logged in',
+    es: 'Inició sesión'
+  }
+
+  Greetr.prototype = {
+    fullName : function() {
+      return this.firstName + ' ' + this.lastName;
+    },
+
+    validate: function(){
+      if (supportedLangs.indexOf(this.language) === -1) {
+        throw "Invalid Language";
+      }
+    },
+
+    greeting: function(){
+      return greetins[this.language] + ' ' + this.firstName + '!';
+    },
+
+    formalGreeting: function() {
+      return formalGreetings[this.language] + ',' + this.fullName();
+    },
+
+    greet: function(formal) {
+      var msg;
+        // if undefined or null it wil be coerced to 'false'
+        if (formal) {
+          msg = this.formalGreeting();
+        }
+        else {
+          msg = this.greeting();
+        }
+        if (console) {
+          console.log(msg);
+        }
+        //'this' refer to the calling object at execution time
+        // makes the method chainable
+        return this;
+    },
+    log: function(){
+      if (console) {
+        console.log(logMessages[this.language] + ': ' +
+         this.fullName());
+      }
+      return this;
+    }
+  };
 
   Greetr.init = function(firstName, lastName, language) {
     var self = this;
