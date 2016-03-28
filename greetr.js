f5e2a9a18400e1a071d5729137b211669be96882
Cody Condon <codycondon50@gmail.com>
2016-03-28 11:02:43 -0400

    creating init

--- greetr.js
+++ greetr.js
@@ -1,5 +1,16 @@
 (function(global, $) {
-  
+
+  var Greetr = function(firstname,lastname,language) {
+    return new Greetr.init(firstName, lastName, language);
+  }
+
+  Greetr.init = function(firstName, lastName, language) {
+    var self = this;
+    self.firstName = firstName || '';
+    self.lastName = lastName || '';
+    self.language = language || 'en';
+
+  }
 
 
 }(window,jQuery));
